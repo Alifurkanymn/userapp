4bf51020d5c208b6357fd1be77f71207a35bb0c9
alifurkanymn <alifurkanymn@gmail.com>
2021-03-09 23:40:46 +0300

    Proje eklendi

---
+++ WpfApplication3/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SQLite;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace WpfApplication3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        class customerDataGrid
+        {
+            public static DataView ItemsSource { get; internal set; }
+        }
+
+        SQLiteConnection Connect = new SQLiteConnection("Data Source=C:/PersonsDB.db;Version=3;");
+        public void list()
+        {
+            
+            
+
+        }
+
+        
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 addwindow = new Window1();
+            addwindow.Show();
+        }
+
+        
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Connect.Open();
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter("SELECT * FROM User", Connect);
+            DataTable Table = new DataTable();
+            adapter.Fill(Table);
+            customerDataGrid.ItemsSource = Table.DefaultView;
+            Connect.Close();
+        }
+    }
+}
---
+++ WpfApplication3/Window1.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SQLite;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+namespace WpfApplication3
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            SQLiteConnection Connect = new SQLiteConnection("Data Source=C:/PersonsDB.db;Version=3;");
+            Connect.Open();
+
+            string sql = "INSERT INTO User(Name,SurName,PhoneNumber,Gender) values(@Name,@SurName,@PhoneNumber,@Gender)";
+            SQLiteCommand komut = new SQLiteCommand(sql,Connect);
+           // komut.Parameters.AddWithValue("@Name",txtName.Text);
+           // komut.Parameters.AddWithValue("@Name", txtSurName.Text);
+           // komut.Parameters.AddWithValue("@Name", txtPhoneNumber.Text);
+           // komut.Parameters.AddWithValue("@Name", txtName.Text);
+           // komut.Parameters.AddWithValue("@Name", txtName.Text);
+        }
+    }
+}
